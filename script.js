94448e06a27a0cf8003df58f80aa543d27827399
tong <z1230cn@163.com>
2025-10-08 08:51:02 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let box = document.querySelectorAll(".box");
+let box1 = box[0];
+let list = document.querySelectorAll(".list")[0];
+let gIdx = 0;
+let colors = [
+    'red',
+    'yellow',
+    'blue',
+    'purple'
+];
+
+box1.onclick = function() {
+    if (gIdx === colors.length) gIdx = 0;
+    box1.style.borderColor = colors[gIdx];
+    box1.style.backgroundColor = colors[gIdx];
+    gIdx++;
+}
+
+for (let idx in colors) {
+    list.innerHTML += `<div class="color" style="color: ${colors[idx]}">${colors[idx]}</div>`
+}
